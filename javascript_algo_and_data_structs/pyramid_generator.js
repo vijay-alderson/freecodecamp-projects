4c2560f9e70e3b7be2364172e6fc1f3f18e4ddb1
Vijay Anantharaman <vijayalderson@protonmail.com>
2025-01-30 19:40:14 +0100

    Add UNFINISHED javascript pyramid generator code

---
+++ javascript_algo_and_data_structs/pyramid_generator.js
@@ -0,0 +1,14 @@
+function padRow(){
+	
+}
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i+1) {
+	rows.push(character.repeat(i+1)); // repeat method will repeat the target string 
+}
+
+const call = padRow(); // use const to assign a function call to the variable. Let doesn't work?
+console.log(call);
