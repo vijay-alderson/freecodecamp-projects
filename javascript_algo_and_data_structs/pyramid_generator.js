62c80f8bb9eea35c2c217df952efbee2e3229172
Vijay Anantharaman <vijayalderson@protonmail.com>
2025-02-02 12:19:10 +0100

    add index.html file

--- javascript_algo_and_data_structs/pyramid_generator.js
+++ javascript_algo_and_data_structs/pyramid_generator.js
@@ -4,10 +4,11 @@ function padRow(){
 
 const character = "#";
 const count = 8;
-const rows = [];
+// const rows = [];
 
 for (let i = 0; i < count; i = i+1) {
-	rows.push(character.repeat(i+1)); // repeat method will repeat the target string 
+	// rows.push(character.repeat(i+1)); // repeat method will repeat the target string 
+	console.log(character.repeat(i+1));
 }
 
 const call = padRow(); // use const to assign a function call to the variable. Let doesn't work?
